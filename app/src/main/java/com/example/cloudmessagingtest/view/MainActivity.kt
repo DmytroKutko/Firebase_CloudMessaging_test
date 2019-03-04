58ce04da4f83b2bbb99e7891712821908057e664
Dmytro Kutko <dmytrokutko@gmail.com>
2019-03-04 11:05:09 +0200

    add menu and sign out method

--- app/src/main/java/com/example/cloudmessagingtest/view/MainActivity.kt
+++ app/src/main/java/com/example/cloudmessagingtest/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.cloudmessagingtest.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.cloudmessagingtest.R
 import com.example.cloudmessagingtest.view.AuthFragments.MainFragment
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +23,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setInitialData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        when (id) {
+            R.id.menuSignOut -> {
+                mAuth.signOut()
+                finish()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setInitialData() {
